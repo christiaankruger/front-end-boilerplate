57f0d87d445c1c3b0b632ed4de0c9aad40ab0175
Christiaan Kruger <c@hristiaan.co.za>
2020-02-18 13:25:57 +0200

    Hello, Storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = ({ config, mode }) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: [['react-app', { flow: false, typescript: true }]]
+    }
+  });
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: [
+      'style-loader', // creates style nodes from JS strings
+      'css-loader', // translates CSS into CommonJS
+      'sass-loader' // compiles Sass to CSS, using Node Sass by default
+    ]
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
+
+  return config;
+};
